27c11f4b6c3d19da1a9617c7f98e8eb2baca1ba5
Wellington Albino <well.renato@hotmail.com>
2023-06-30 12:39:16 -0300

    corrigindo bug

--- src/components/AuthButtons/index.tsx
+++ src/components/AuthButtons/index.tsx
@@ -11,12 +11,13 @@ type AuthButtonsProps = {
 export const AuthButtons = ({ canGuest, callbackUrl = '/' }: AuthButtonsProps) => {
   const router = useRouter()
 
-  const handleSignIn = (provider?: string, e?: any) => {
+  const handleSignIn = (e?: any, provider?: string) => {
+    e.preventDefault()
     if(!provider) {
       router.push(callbackUrl)
       return
     }
-    e.preventDefault()
+    
     signIn(provider, {
       callbackUrl
     })
